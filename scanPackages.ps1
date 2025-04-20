d7b5ab4c4f4e15dd4a236a6a58b02b8aa922def0
Kristoffer Johansson <kjohansson93@hotmail.se>
2025-04-20 17:35:01 +0200

    Testing.

---
+++ scanPackages.ps1
@@ -0,0 +1,23 @@
+param(
+	[Parameter(mandatory)]
+	[string]$rootDirectory
+)
+
+$SOLUTIONROOT = "$rootDirectory"
+Function ListAllPackages ($BaseDirectory)
+
+{
+    Write-Host "Starting Package List - This may take a few minutes ..."
+    $PACKAGECONFIGS = Get-ChildItem -Recurse -Force $BaseDirectory -ErrorAction SilentlyContinue |
+        Where-Object { ($_.PSIsContainer -eq $false) -and  ( $_.Name -eq "packages.config")}
+    ForEach($PACKAGECONFIG in $PACKAGECONFIGS)
+    {
+        $path = $PACKAGECONFIG.FullName
+        $xml = [xml]$packages = Get-Content $path
+        foreach($package in $packages.packages.package)
+        {
+            Write-Host "$($package.id) - $($package.version)"
+        }
+    }
+}
+ListAllPackages $SOLUTIONROOT
